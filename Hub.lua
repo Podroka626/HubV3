2a6e16fffa4c9f1ff3e6565bf8383820756e305c
WOLF SCRIPTS <123605649+Podroka626@users.noreply.github.com>
2023-04-03 13:45:59 -0300

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,11 +1,11 @@
 local StarterGui = game:GetService("StarterGui")
-local Raw = "https://github.com/Podroka626/HubV3/tree/main/Games/"..game.PlaceId..".lua"
+local Script = game:HttpGet("https://raw.githubusercontent.com/Podroka626/HubV3/main/Games/"..game.PlaceId..".lua")
 
-if Raw then
+if Script then
   StarterGui:SetCore("SendNotification", {
         Title = "Wolf Hub",
         Text = "Loading script.",
         Duration = 5
     })
-    loadstring(game:HttpGet(Raw))
+    loadstring(Script)()
 end
